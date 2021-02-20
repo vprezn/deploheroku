1f45d9bd3d7b7efc892a822aa6d4b9b428fef73b
vprezn <79380777+vprezn@users.noreply.github.com>
2021-02-21 01:36:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -30,28 +30,27 @@ def home():
 def predict():
     if request.method == "POST":
         data = request.get_json()
-        return jsonify({'success': data})
-#         return jsonify({ 
-#             'success': True,
-#             'data': model.predit([
-#                 data["gender"],
-#                 data["Nationalty"],
-#                 data["place_of_birth"],
-#                 data["stage"],
-#                 data["grade"],
-#                 data["section"],
-#                 data["topic"],
-#                 data["semester"],
-#                 data["relation"],
-#                 data["raisedhands"],
-#                 data["visted_resource"],
-#                 data["AnnouncementsView"],
-#                 data["Discussion"],
-#                 data["ParentAnsweringSurvey"],
-#                 data["ParentschoolSatisfaction"],
-#                 data["StudentAbsenceDays"],
-#                 ]),
-#             })
+        return jsonify({ 
+            'success': True,
+            'data': model.predit([[
+                data["gender"],
+                data["Nationalty"],
+                data["place_of_birth"],
+                data["stage"],
+                data["grade"],
+                data["section"],
+                data["topic"],
+                data["semester"],
+                data["relation"],
+                data["raisedhands"],
+                data["visted_resource"],
+                data["AnnouncementsView"],
+                data["Discussion"],
+                data["ParentAnsweringSurvey"],
+                data["ParentschoolSatisfaction"],
+                data["StudentAbsenceDays"],
+                ]]),
+            })
 
 
 if __name__ == "__main__":
