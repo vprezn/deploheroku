46f8a8e7cd9cf46d7306ce722f511547bf0979a6
vprezn <79380777+vprezn@users.noreply.github.com>
2021-02-21 01:20:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,27 +29,28 @@ def home():
 @app.route('/student_performance_prediction',methods=['GET','POST'])
 def predict():
     data = request.get_json()
+    data = request.get_json()
     return jsonify({
-                'success': True,
-                'data': model.predit([
-                            data["gender"],
-                            data["Nationalty"],
-                            data["place_of_birth"],
-                            data["stage"],
-                            data["grade"],
-                            data["section"],
-                            data["topic"],
-                            data["semester"],
-                            data["relation"],
-                            data["raisedhands"],
-                            data["visted_resource"],
-                            data["AnnouncementsView"],
-                            data["Discussion"],
-                            data["ParentAnsweringSurvey"],
-                            data["ParentschoolSatisfaction"],
-                            data["StudentAbsenceDays"],
-                        ]),
-            })
+        'success': True,
+        'data': model.predit([
+            data["gender"],
+            data["Nationalty"],
+            data["place_of_birth"],
+            data["stage"],
+            data["grade"],
+            data["section"],
+            data["topic"],
+            data["semester"],
+            data["relation"],
+            data["raisedhands"],
+            data["visted_resource"],
+            data["AnnouncementsView"],
+            data["Discussion"],
+            data["ParentAnsweringSurvey"],
+            data["ParentschoolSatisfaction"],
+            data["StudentAbsenceDays"],
+            ]),
+        })
 
 
 if __name__ == "__main__":
