04549a967efa753147219f4cf5d9c064b4c95f4d
vprezn <79380777+vprezn@users.noreply.github.com>
2021-02-23 16:29:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ import tensorflow_hub as hub
 from PIL import Image
 #Initialize the flask App
 app = Flask(__name__)
-# cors = CORS(app, resources={r"/api/": {"origins": ""}})
-cors = CORS(app)
+cors = CORS(app, resources={r"/api/": {"origins": ""}})
+# cors = CORS(app)
  
-# CORS Headers
-# @app.after_request
-# def after_request(response):
-#     header = response.headers
-#     header['Access-Control-Allow-Origin'] = '*'
-#     header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-#     header['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET, POST, DELETE, PUT'
-#     return response
+CORS Headers
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET, POST, DELETE, PUT'
+    return response
 
 student_performance_logistic_model = pickle.load(open('student_performance_logistic_model.pkl', 'rb'))
 land_price_prediction_ridge_model = pickle.load(open('land_price_prediction_ridge_model.pkl', 'rb'))
